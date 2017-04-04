7f6bef36488f1a3a5af6dae237be8e67780664d7
Laurent Domenech-Cabaud <laurent.dc@galepartners.com>
2017-04-04 14:24:39 -0400

    removed options

--- {{cookiecutter.project_slug}}/django/apps/tasks/celery.py
+++ {{cookiecutter.project_slug}}/django/apps/tasks/celery.py
@@ -26,34 +26,6 @@ class CeleryConfig(AppConfig):
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
 
-        {% if cookiecutter.use_sentry_for_error_reporting == 'y' -%}
-        if hasattr(settings, 'RAVEN_CONFIG'):
-            # Celery signal registration
-            from raven import Client as RavenClient
-            from raven.contrib.celery import register_signal as raven_register_signal
-            from raven.contrib.celery import register_logger_signal as raven_register_logger_signal
-
-            raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['DSN'])
-            raven_register_logger_signal(raven_client)
-            raven_register_signal(raven_client)
-        {%- endif %}
-
-        {% if cookiecutter.use_opbeat == 'y' -%}
-        if hasattr(settings, 'OPBEAT'):
-            from opbeat.contrib.django.models import client as opbeat_client
-            from opbeat.contrib.django.models import logger as opbeat_logger
-            from opbeat.contrib.django.models import register_handlers as opbeat_register_handlers
-            from opbeat.contrib.celery import register_signal as opbeat_register_signal
-
-            try:
-                opbeat_register_signal(opbeat_client)
-            except Exception as e:
-                opbeat_logger.exception('Failed installing celery hook: %s' % e)
-
-            if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
-                opbeat_register_handlers()
-        {%- endif %}
-
 
 @app.task(bind=True)
 def debug_task(self):
